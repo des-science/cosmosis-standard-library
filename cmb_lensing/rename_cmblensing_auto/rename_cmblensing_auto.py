ed4ba92df564ccb5aaad622efd9ecd2dd695b9c2
Eric Baxter <ericjbax@gmail.com>
2023-04-05 18:26:47 -0700

    fixing cmb lensing auto convention, making sure it's sufficiently accurate, and renaming scale cut file

--- cmb_lensing/rename_cmblensing_auto/rename_cmblensing_auto.py
+++ cmb_lensing/rename_cmblensing_auto/rename_cmblensing_auto.py
@@ -3,14 +3,21 @@ import numpy as np
   
 def setup(options):
     output_name = options.get_string(option_section, "output_name", default="cmbkappa_cl")
-    return {'output_name':output_name}
+    convert_to_convergence = options.get_bool(option_section, "convert_to_convergence", default=True)
+    return {'output_name':output_name, 'convert_to_convergence':convert_to_convergence}
 
 def execute(block, config):
     ell = block[names.cmb_cl,'ell']
-    Clkk = block[names.cmb_cl,'pp']
+    Cl_block = block[names.cmb_cl,'pp'] # this is Clpp*(ell*(ell+1))/2pi
+    if config['convert_to_convergence']:
+        Clpp = Cl_block*2.*np.pi/(ell*(ell+1.))
+        Clkk = Clpp*(1./4.)*(ell*(ell+1.))**2.
+        block[config['output_name'],'bin_1_1'] = Clkk
+    else:
+        block[config['output_name'],'bin_1_1'] = Cl_block
+        
     block[config['output_name'],'ell'] = ell
     block[config['output_name'],'sep_name'] = 'ell'         
-    block[config['output_name'],'bin_1_1'] = Clkk
     block[config['output_name'],'is_auto'] = True
     block[config['output_name'],'nbin'] = 1
     block[config['output_name'],'nbin_a'] = 1
