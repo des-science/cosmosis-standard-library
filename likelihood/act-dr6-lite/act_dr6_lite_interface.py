74157077a00616277b3fe1db34e6a8cb84f74d86
Sujeong Lee <lemoncookie3@gmail.com>
2025-09-04 14:43:27 -0700

    act likelihood now can take synthetic data

--- likelihood/act-dr6-lite/act_dr6_lite_interface.py
+++ likelihood/act-dr6-lite/act_dr6_lite_interface.py
@@ -2,7 +2,7 @@ try:
     import act_dr6_cmbonly
 except ImportError:
     raise RuntimeError('The act_dr6_cmbonly python module is required for the act_dr6_lite likelihood. It can be obtained from https://github.com/ACTCollaboration/DR6-ACT-lite')
-from cosmosis.datablock import names
+from cosmosis.datablock import names, option_section
 cosmo = names.cosmological_parameters
 import numpy as np
 import os
@@ -15,9 +15,32 @@ cal_params = [
 
 dirname = os.path.split(__file__)[0]
 
-
 def setup(options):
     act = act_dr6_cmbonly.ACTDR6CMBonly(packages_path=dirname)
+
+    # cosmoSIS theory output 
+    sim_data_directory = options.get_string(option_section, 'use_data_from_test', default='')
+
+    # replace real data with synthetic data
+    if sim_data_directory != '':
+        sim_ell = np.genfromtxt( sim_data_directory + 'cmb_cl/ell.txt')
+        f1 = 1.0 #sim_ell * (sim_ell + 1) / (2 * np.pi)
+        sim_cl_tt = np.genfromtxt( sim_data_directory + 'cmb_cl/tt.txt') / f1
+        sim_cl_te = np.genfromtxt( sim_data_directory + 'cmb_cl/te.txt') / f1
+        sim_cl_ee = np.genfromtxt( sim_data_directory + 'cmb_cl/ee.txt') / f1
+        sim_cl_dict = {'tt':sim_cl_tt, 'te':sim_cl_te, 'ee':sim_cl_ee}
+        nell = sim_ell.shape
+
+        ps_vec = np.zeros_like(act.data_vec)
+        for m in act.spec_meta:
+            idx = m["idx"]
+            win = m["window"].weight.T
+            ls = m["window"].values
+            pol = m["pol"]
+            dat = sim_cl_dict[pol][ls]
+            ps_vec[idx] = win @ dat
+        act.data_vec = ps_vec
+
     return act
 
 
@@ -35,7 +58,7 @@ def execute(block, config):
         # https://github.com/ACTCollaboration/DR6-ACT-lite/issues/12 
         # when a_planck is found in value.ini, A_act is fixed to a_planck
         block["act_params", "A_act"] = block["planck", "a_planck"]
-
+    
     nuisance = {}
 
     for p in cal_params:
