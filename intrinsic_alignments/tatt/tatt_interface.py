ec5196d4b3513192df016a459de11d6017c1562e
Sujeong Lee <lemoncookie3@gmail.com>
2026-08-03 18:39:12 -0400

    tatt module can rename ia section

--- intrinsic_alignments/tatt/tatt_interface.py
+++ intrinsic_alignments/tatt/tatt_interface.py
@@ -8,7 +8,6 @@ from des_ia_lib.common import resample_power
 import numpy as np
 
 # We now return you to your module.
-import ipdb
 
 from cosmosis.datablock import names, option_section
 import scipy.interpolate as interp
@@ -250,6 +249,7 @@ def setup(options):
     sub_lowk = options.get_bool(option_section, "sub_lowk", False)
     ia_model = options.get_string(option_section, "ia_model", "nla")
     name = options.get_string(option_section, "name", default="").lower()
+    param_name = options.get_string(option_section, "param_name", default="").lower()
     do_galaxy_intrinsic = options.get_bool(option_section, "do_galaxy_intrinsic", False)
     no_IA_E = options.get_bool(option_section, "no_IA_E", False)
     no_IA_B = options.get_bool(option_section, "no_IA_B", False)
@@ -260,10 +260,17 @@ def setup(options):
         suffix = "_" + name
     else:
         suffix = ""
+
+    if param_name:
+        param_name = '_' + param_name
+    else:
+        param_name = ""
+
     return (
         sub_lowk,
         ia_model,
         suffix,
+        param_name,
         do_galaxy_intrinsic,
         no_IA_E,
         no_IA_B,
@@ -277,6 +284,7 @@ def execute(block, config):
         sub_lowk,
         ia_model,
         suffix,
+        param_name,
         do_galaxy_intrinsic,
         no_IA_E,
         no_IA_B,
@@ -319,7 +327,7 @@ def execute(block, config):
     E_factor = 0 if no_IA_E else 1
     B_factor = 0 if no_IA_B else 1
 
-    ia_section = "intrinsic_alignment_parameters"
+    ia_section = "intrinsic_alignment_parameters" + param_name
 
     # check for deprecated parameters
     if (ia_section, "C1") in block:
